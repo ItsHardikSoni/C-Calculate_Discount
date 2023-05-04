d1d3d3206631028592a8aff3e15b07dc7afcee67
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-04 12:21:00 +0530

    Create Program.c

---
+++ Program.c
@@ -0,0 +1,22 @@
+// While purchasing certain items, 
+// a discount of 10% is offered if the quantity purchased is more than 1000.
+// If quantity and price per item are input through the keyboard, 
+// write a program to calculate the total expenses.
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int qty, dis;
+  float rate, tot;
+  printf("Enter quantity and rate");
+  scanf("%d %f", &qty, &rate);
+  if (qty > 1000)
+    dis = 10;
+  else
+    dis = 0;
+  tot = ( qty * rate)-( qty rate * dis / 100); 
+  printf("Total expenses = Rs. %f\n", tot);
+  return 0;
+}
